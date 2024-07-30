cb23221881752c2ce1458ba1a914f8f21a152b0d
Sam Hill <124188098+locustcode@users.noreply.github.com>
2024-07-30 16:25:39 -0400

    Fixed iOS zooming in on input

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+if (navigator.userAgent.indexOf("iPhone") > -1) {
+  document
+    .querySelector("[name=viewport]")
+    .setAttribute(
+      "content",
+      "width=device-width, initial-scale=1, maximum-scale=1"
+    );
+}
+
 let leagueSize = 12;
 document.getElementById("active-text").textContent = leagueSize;
 
